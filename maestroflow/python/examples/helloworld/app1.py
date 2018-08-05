b8aa7032f468d6fd3fe31cdb0b4b3011534205ce
Caleb Helbling <caleb.helbling@gmail.com>
2018-08-05 02:35:10 -0400

    Added comments to hello world examples

--- maestroflow/python/examples/helloworld/app1.py
+++ maestroflow/python/examples/helloworld/app1.py
@@ -1,9 +1,12 @@
 import maestroflow.python.api as mf
 import signal
 
+# Create a new application running on port 13377 called app1, and use the given icon
 app = mf.Application(13377, "app1", mf.image('../twirl-icon.png'))
+# Create a source called app1.source which broadcasts data of type text
 source = mf.Source(app, "app1.source", "text")
 
+# A signal to kill the program should also kill the MaestroFlow application
 def signal_handler(sig, frame):
     app.stop()
     sys.exit(0)
@@ -11,4 +14,5 @@ signal.signal(signal.SIGINT, signal_handler)
 
 while True:
     txt = input("Enter some text to send: ")
+    # Notify MaestroFlow that we got an input
     source.notify(txt)
--- maestroflow/python/examples/helloworld/app2.py
+++ maestroflow/python/examples/helloworld/app2.py
@@ -2,15 +2,20 @@ import maestroflow.python.api as mf
 import signal
 import time
 
+# Create a new application running on port 13378 called app2, and use the unicorn icon
 app = mf.Application(13378, "app2", mf.image('../unicorn-icon.png'))
+# Create a sink called app2.sink which accepts events of type text
 s = mf.Sink(app, "app2.sink", "text")
+# Register the callback handler with the sink. In this case we directly print the data
 s.on_notify(print)
 
+# A signal to kill the program should also kill the MaestroFlow application
 def signal_handler(sig, frame):
     app.stop()
     sys.exit(0)
 signal.signal(signal.SIGINT, signal_handler)
 
 while True:
+    # For the MaestroFlow sinks to function, we have to occasionally call poll
     app.poll()
     time.sleep(0.01)
